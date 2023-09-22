4dce85b144f4584e17a22520345d19d28e06b8e3
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-22 16:50:39 -0300

    add sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import styles from './Sidebar.module.css';
+
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover}
+            src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+            />
+            <div className={styles.profile}>
+                <strong>Breno Costa</strong>
+                <span>WEB DEVELOPER</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
