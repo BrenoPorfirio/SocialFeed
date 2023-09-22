38bb3215cfcbbbcfd95360006dc3f54e68b1cb09
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-21 21:18:02 -0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Header } from './components/Header';
 import './styles.css';
 
 function App() {
 
   return (
-    <h1>Rocketseat</h1>
+    <div>
+      <Header/>
+      <h1>Rocketseat</h1>
+    </div>
   )
 }
 
