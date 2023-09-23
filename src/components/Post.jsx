c5e104e356265337e07d14f6588737941bc02155
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 20:22:05 -0300

    correction in function

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import styles from './Post.module.css';
 
 export function Post () {
     return (
-        <article className={styles.Post}>
+        <article className={styles.post}>
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https:github.com/BrenoPorfirio.png" />
@@ -16,9 +16,13 @@ export function Post () {
 
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉<a href="">jane.design/doctorcare</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare🚀</p>
+                <p><a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>{' '}
+                </p>
             </div>
         </article>
     )
