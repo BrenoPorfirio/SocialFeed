e0067d8b9cd9008994d1a01867ae43def0484dc1
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 19:12:13 -0300

    add pencil in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img className={styles.cover}
+            <img 
+            className={styles.cover}
             src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
             />
+
             <div className={styles.profile}>
                 <img className={styles.avatar} src="https:github.com/BrenoPorfirio.png"
                 />
@@ -16,6 +19,7 @@ export function Sidebar() {
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
