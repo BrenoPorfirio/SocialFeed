2dc0a3c8d256002669e0b8f3402c0a6704fa04c1
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 18:56:41 -0300

    add avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,8 @@ export function Sidebar() {
             src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https:github.com/BrenoPorfirio.png"
+                />
                 <strong>Breno Costa</strong>
                 <span>WEB DEVELOPER</span>
             </div>
