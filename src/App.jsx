e10438d7f384ff1fd63e945e79811bdc06794111
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 19:54:14 -0300

    add post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 import './global.css';
 import styles from './App.module.css';
-import { Sidebar } from './components/Sidebar';
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
+          <Post
+          />
 
         </main>
       </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,25 @@
+import styles from './Post.module.css';
+
+export function Post () {
+    return (
+        <article className={styles.Post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https:github.com/BrenoPorfirio.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Breno Costa</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title="23 de setembro às 19:45h" dateTime="2023-23-09 19:45:50">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉<a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+}
