dacb300119e86cc83f4826edfe3e3a18fc960098
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 20:29:20 -0300

    add fomr

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -24,6 +24,16 @@ export function Post () {
                     <a href="">#rocketseat</a>{' '}
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder="Deixe um comentário"
+                />
+
+                <button type="submit">Comentar</button>
+            </form>
         </article>
     )
 }
