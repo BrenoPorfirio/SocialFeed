ef78a6fb98a869e71ec9dd0d6bf06ef9cf2a2b3d
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 21:36:20 -0300

    add comments in feed

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post () {
@@ -35,6 +36,12 @@ export function Post () {
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+            <Comment />
+            <Comment />
+            <Comment />
+            </div>
         </article>
     )
 }
