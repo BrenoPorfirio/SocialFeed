208fff9302f3677da515201ef5c3650530043b46
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 22:35:17 -0300

    add avatar DD and prop on border

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://media.licdn.com/dms/image/C4D03AQE0LX2FPT2RSg/profile-displayphoto-shrink_200_200/0/1642603359775?e=1701302400&v=beta&t=iSm4ukNbTmRP9CsOHc3P3hsYerISsOX2EZHtr87g5HM"
+            <Avatar hasBorder={false} src="https://media.licdn.com/dms/image/C4D03AQE0LX2FPT2RSg/profile-displayphoto-shrink_200_200/0/1642603359775?e=1701302400&v=beta&t=iSm4ukNbTmRP9CsOHc3P3hsYerISsOX2EZHtr87g5HM"
             />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
