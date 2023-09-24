da349915063df06c862a99a6bef04094df62798d
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-24 14:34:40 -0300

    iterating in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,36 @@ import { Sidebar } from './components/Sidebar';
 import './global.css';
 import styles from './App.module.css';
 
+const posts =[
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/BrenoPorfirio.png',
+      name: 'Breno Porfirio',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare🚀', },
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-09-23 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'http://github.com/igwbriel.png',
+      name: 'José Gabriel',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare🚀', },
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-09-24 20:00:00')
+  },
+]
 
 function App() {
 
