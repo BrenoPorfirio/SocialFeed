b0ce3975e810584bc60f1487243a7ff61820fb07
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 22:36:41 -0300

    apllying destructuring

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,10 @@
 import styles from './Avatar.module.css';
 
-export function Avatar(props) {
+export function Avatar({ hasBorder = true, src}) {
+
     return (
-        <img className={styles.avatar} src={props.src}
+        <img className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+        src={src}
         />
     );
 }
