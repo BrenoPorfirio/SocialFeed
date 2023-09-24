06e569dd90ac63a37011dc5796c2c90358aa5653
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 21:54:49 -0300

    change size

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -15,7 +15,7 @@ export function Comment() {
                         </div>
 
                         <button title="Deletar comentário">
-                            <Trash size={20}/>
+                            <Trash size={24}/>
                         </button>
                     </header>
 
