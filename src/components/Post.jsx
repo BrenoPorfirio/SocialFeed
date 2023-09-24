9c1d2f30d068ab537802f3c973a9c2e521e36d71
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-24 15:15:30 -0300

    utilizing map in content

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,7 @@ import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 
-export function Post ({ author, publishedAt }) {
+export function Post ({ author, publishedAt, content }) {
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -30,14 +30,13 @@ export function Post ({ author, publishedAt }) {
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare🚀</p>
-                <p><a href="">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>{' '}
-                </p>
+                {content.map(line =>{
+                    if (line.type == 'paragraph'){
+                        return <p>{line.content}</p>
+                    } else if (line.type == 'link'){
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
