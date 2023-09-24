fac9c1bad691e759189b2b58e989fc82fbaa4740
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-23 22:22:37 -0300

    profile BrenoP on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 
@@ -11,10 +12,9 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https:github.com/BrenoPorfirio.png"
-                />
+            <Avatar src="https:github.com/BrenoPorfirio.png"/>
                 <strong>Breno Costa</strong>
-                <span>WEB DEVELOPER</span>
+                <span>Web Developer</span>
             </div>
 
             <footer>
