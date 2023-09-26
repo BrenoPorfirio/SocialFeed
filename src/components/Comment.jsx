7601c078e43b19de785dcc078fce42aaf20ca73e
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-25 23:43:22 -0300

    finish project

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -18,14 +18,17 @@ export function Comment({ content, ondeleteComment }) {
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://media.licdn.com/dms/image/C4D03AQE0LX2FPT2RSg/profile-displayphoto-shrink_200_200/0/1642603359775?e=1701302400&v=beta&t=iSm4ukNbTmRP9CsOHc3P3hsYerISsOX2EZHtr87g5HM"
+            <Avatar hasBorder={false}
+            src="https://media.licdn.com/dms/image/C4D03AQE0LX2FPT2RSg/profile-displayphoto-shrink_200_200/0/1642603359775?e=1701302400&v=beta&t=iSm4ukNbTmRP9CsOHc3P3hsYerISsOX2EZHtr87g5HM"
             />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
                             <strong>Daiane Dias</strong>
-                            <time title="23 de setembro às 19:45h" dateTime="2023-23-09 19:45:50">Cerca de 1h atrás</time>
+                            <time title="23 de setembro às 19:45h"
+                            dateTime="2023-23-09 19:45:50"
+                            >Cerca de 1h atrás</time>
                         </div>
 
                         <button onClick={handleDeleteComment} title="Deletar comentário">
